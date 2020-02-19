6dd1dbfa3adf9b2c927e245e66dba8aec7382a0a
Maurizio Monti <maurimonti@msn.com>
2020-02-19 15:49:33 +0000

    Add files via upload

--- Mobility.py
+++ Mobility.py
@@ -1,4 +1,5 @@
 import numpy as np
+# import matplotlib.pyplot as plt
 import scipy.constants as cnst
 import tkinter as tk
 from tkinter import ttk
@@ -22,7 +23,7 @@ def calculate(*args):
         En.set((P_val * 1e-3 / R_val) * 1e6)
         En_val = float(En.get())
 
-        F.set(En_val / (((d_val * 1e-1)**2 + 0.03**2) * pi * 2))
+        F.set(En_val / (((d_val * 1e-1)**2) * pi * 2))
         Ep.set(h * c / (wl_val * 1e-9) / e)
 
         Ep_val = float(Ep.get())
@@ -35,7 +36,7 @@ def calculate(*args):
         ns_val = float(ns.get())
         dEonE_val = float(dEonE.get())
         # Abs_val = float(Abs.get())
-        phi_abs_val = phi_val
+        phi_abs_val = phi_val /  (1 + 0.03**2 / d_val**2)
 
         mu.set((ni_val + ns_val) * dEonE_val / (Z0 * e * phi_abs_val * 1e14))
     except ValueError:
@@ -43,7 +44,7 @@ def calculate(*args):
 
 
 root = tk.Tk()
-root.title('Mobility')
+root.title('Example')
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
@@ -79,6 +80,8 @@ ns_entry = ttk.Entry(mainframe, width=7, textvariable=ns)
 ns_entry.grid(column=1, row=6, sticky=(W, E))
 dEonE_entry = ttk.Entry(mainframe, width=7, textvariable=dEonE)
 dEonE_entry.grid(column=1, row=7, sticky=(W, E))
+# Abs_entry = ttk.Entry(mainframe, width=7, textvariable=Abs)
+# Abs_entry.grid(column=1, row=4, sticky=(W, E))
 
 ttk.Label(mainframe,
           textvariable=En).grid(column=3, row=2, sticky=(W, E))
@@ -103,6 +106,13 @@ ttk.Label(mainframe, text="Wavelength(nm)").grid(column=2, row=4, sticky=W)
 ttk.Label(mainframe, text="ni").grid(column=2, row=5, sticky=W)
 ttk.Label(mainframe, text="ns").grid(column=2, row=6, sticky=W)
 ttk.Label(mainframe, text="Delta E /E").grid(column=2, row=7, sticky=W)
+# ttk.Label(mainframe, text="Absorbance").grid(column=2, row=8, sticky=W)
+
+
+# E_entry = ttk.Entry(mainframe, width=7, textvariable=En)
+# E_entry.grid(column=3, row=1, sticky=('W', 'E'))
+# F_entry = ttk.Entry(mainframe, width=7, textvariable=F)
+# F_entry.grid(column=3, row=2, sticky=('W', 'E'))
 
 ttk.Label(mainframe, text="Energy pp(uJ):").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="Fluence(uJ/cm^2):").grid(column=3, row=3, sticky=W)
@@ -117,5 +127,39 @@ for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
 P_entry.focus()
 root.bind('<Return>', calculate)
+
+
+# d = 1.36  # mm
+# R = 500  # Hz
+# wl = 800  # nm
+# P = 1  # mW
+# ni = 1.0
+# ns = 2.0
+# Re = np.abs((3.4 - 1) / (3.4 + 1))
+# A = 3.5
+# dEonE = 0.12
+
+# E = P * 1e-3 / R
+# print('Energy per pulse:')
+# print(str(E * 1e6) + ' uJ\n')
+# F = E / ((d * 1e-1)**2 * pi)
+# print('Fluence:')
+# print(str(F * 1e6) + ' uJ/cm^2\n')
+# E_p = h * c / (wl * 1e-9)
+# print('Photon Energy:')
+# print(str(E_p / e) + ' eV\n')
+# phi = F / E_p
+# print('Photon flux:')
+# print(str(phi * 1e-14) + ' x10$^{14}$ 1/cm^2\n')
+# phi_abs = phi * (1 - Re) * (1 - 10**(-A))
+# print('Absorbed photons:')
+# print(str(phi_abs * 1e-14) + ' x10$^{14}$ 1/cm^2\n')
+
+# # mu = (h * c * R * (d * 1e-1)**2 * pi * (ni + ns)) * dEonE /\
+# #     (e * Z0 * wl * 1e-9 * P * 1e-3 * (1 - Re) * (1 - 10**(-A)))
+
+# mu = (ni + ns) * dEonE / (Z0 * e * phi_abs)
+# print('Mobility:')
+# print(s2tr(mu) + ' cm^2/Vs')
 
 root.mainloop()
